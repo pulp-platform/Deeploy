4c36de2bd45e24ba3eaa57eeeac33af1cd9731e4
samanthawangdl <samanthawangdl@gmail.com>
2025-04-24 13:12:22 +0000

    matmul 1*4 unrolling

--- TargetLibraries/Generic/src/MatMul_fp32.c
+++ TargetLibraries/Generic/src/MatMul_fp32.c
@@ -29,20 +29,78 @@
 #include "DeeployBasicMath.h"
 
 void MatMul_fp32_fp32_fp32(const float32_t *__restrict__ pSrcA,
-                               const float32_t *__restrict__ pSrcB,
-                               float32_t *__restrict__ pDstY,
-                               uint32_t M,
-                               uint32_t N, 
-                               uint32_t O) {
+                           const float32_t *__restrict__ pSrcB,
+                           float32_t *__restrict__ pDstY,
+                           uint32_t M,
+                           uint32_t N,
+                           uint32_t O)
+{
 
- 
-  for (uint32_t i = 0; i < M; ++i) {
-    for (uint32_t j = 0; j < O; ++j) {
+  for (uint32_t i = 0; i < M; ++i)
+  {
+    for (uint32_t j = 0; j < O; ++j)
+    {
       float32_t sum = 0.0f;
-      for (uint32_t k = 0; k < N; ++k) {
+      for (uint32_t k = 0; k < N; ++k)
+      {
         sum += pSrcA[i * N + k] * pSrcB[k * O + j];
       }
       pDstY[i * O + j] = sum;
     }
   }
 }
+
+void MatMul_fp32_fp32_fp32_unroll1x4(const float32_t *__restrict__ pSrcA,
+                                     const float32_t *__restrict__ pSrcB,
+                                     float32_t *__restrict__ pDstY,
+                                     uint32_t M,
+                                     uint32_t N,
+                                     uint32_t O)
+{
+  uint32_t i, j, k;
+  uint32_t O_block = O - (O % 4); 
+
+  for (i = 0; i < M; i++)
+  {
+    for (j = 0; j < O_block; j += 4)
+    {
+      float32_t sum0 = 0.0f;
+      float32_t sum1 = 0.0f;
+      float32_t sum2 = 0.0f;
+      float32_t sum3 = 0.0f;
+
+      for (k = 0; k < N; k++)
+      {
+        float32_t a0 = pSrcA[i * N + k];
+
+        float32_t b0 = pSrcB[k * O + (j + 0)];
+        float32_t b1 = pSrcB[k * O + (j + 1)];
+        float32_t b2 = pSrcB[k * O + (j + 2)];
+        float32_t b3 = pSrcB[k * O + (j + 3)];
+
+        sum0 += a0 * b0;
+        sum1 += a0 * b1;
+        sum2 += a0 * b2;
+        sum3 += a0 * b3;
+      }
+
+      pDstY[i * O + (j + 0)] = sum0;
+      pDstY[i * O + (j + 1)] = sum1;
+      pDstY[i * O + (j + 2)] = sum2;
+      pDstY[i * O + (j + 3)] = sum3;
+    }
+
+    for (j = O_block; j < O; j++)
+    {
+      float32_t sum = 0.0f;
+
+      for (k = 0; k < N; k++)
+      {
+        sum += pSrcA[i * N + k] * pSrcB[k * O + j];
+      }
+
+      pDstY[i * O + j] = sum;
+    }
+  }
+}
+
