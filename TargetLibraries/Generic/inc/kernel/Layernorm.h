c51694b57333dd1751f1af2d6accb6fab4036034
Run Wang <52746141+SamanthaWangdl@users.noreply.github.com>
2025-05-12 21:18:25 +0000

    Fix Layernormgrad

--- TargetLibraries/Generic/inc/kernel/Layernorm.h
+++ TargetLibraries/Generic/inc/kernel/Layernorm.h
@@ -45,7 +45,13 @@ void Layernorm_s8_s8(int8_t *data_in, int8_t *data_out, int32_t *weight,
                      int32_t *bias, int32_t input_offset, int32_t size,
                      int32_t lastDimLength, int32_t log2D);
 
-void Layernorm_fp32_fp32(float32_t *data_in, float32_t *data_out, 
+/******************************************************************************/
+/*                             Layernorm (f32bit)                               */
+/******************************************************************************/
+void Layernorm_fp32_fp32(float32_t *data_in, float32_t *data_out,
                          float32_t *scale, float32_t *bias, float32_t epsilon, int32_t size, int32_t lastDimLength);
 
+void LayernormGrad_fp32_fp32(float32_t *grad_in, float32_t *data_in, float32_t *grad_out,
+                             float32_t *scale, float32_t *bias, float32_t epsilon,
+                             int32_t size, int32_t lastDimLength);
 #endif //__DEEPLOY_BASIC_MATH_LAYERNORM_KERNEL_HEADER_
