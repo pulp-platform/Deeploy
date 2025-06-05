8be9bc0acb5a27826b79c328def319c468d72c57
Run Wang <52746141+SamanthaWangdl@users.noreply.github.com>
2025-06-05 20:32:16 +0000

    layernorm update

--- Deeploy/Targets/PULPOpen/Templates/FloatLayernormTemplate.py
+++ Deeploy/Targets/PULPOpen/Templates/FloatLayernormTemplate.py
@@ -2,7 +2,7 @@
 #
 # File: FloatLayernormTemplate.py
 #
-# Last edited: 23.01.2025
+# Last edited: 05.06.2025
 #
 # Copyright (C) 2023, ETH Zurich and University of Bologna.
 #
@@ -26,37 +26,14 @@
 from Deeploy.DeeployTypes import NodeTemplate
 
 referenceTemplate = NodeTemplate("""
-// FloatLayernorm Parallel (Name: ${nodeName}, Op: ${nodeOp})
-
-int8_t ${nodeName}_core_id = pi_core_id();
-int8_t ${nodeName}_log2Core = log2(NUM_CORES);
-
-int32_t ${nodeName}_seq_length = ${size} / ${lastDimLength};
-int32_t ${nodeName}_chunk = (${nodeName}_seq_length >> ${nodeName}_log2Core) + 
-                          ((${nodeName}_seq_length & (NUM_CORES-1)) != 0);
-int32_t ${nodeName}_start = MIN(${nodeName}_chunk * ${nodeName}_core_id, ${nodeName}_seq_length);
-int32_t ${nodeName}_end = MIN(${nodeName}_start + ${nodeName}_chunk, ${nodeName}_seq_length);
-
-
-int32_t ${nodeName}_elem_start = ${nodeName}_start * ${lastDimLength};
-int32_t ${nodeName}_elem_end = ${nodeName}_end * ${lastDimLength};
-int32_t ${nodeName}_elem_count = ${nodeName}_elem_end - ${nodeName}_elem_start;
-
-
-const float* ${nodeName}_data_in_ptr = ${data_in} + ${nodeName}_elem_start;
-float* ${nodeName}_data_out_ptr = ${data_out} + ${nodeName}_elem_start;
-
-
-if (${nodeName}_elem_count > 0) {
-    Layernorm_fp${data_in_type.referencedType.typeWidth}_fp${data_out_type.referencedType.typeWidth}(
-        ${nodeName}_data_in_ptr, 
-        ${nodeName}_data_out_ptr, 
-        ${weight}, 
-        ${bias}, 
-        ${epsilon}, 
-        ${nodeName}_elem_count, 
-        ${lastDimLength}
-    );
-}
-
+// Float Layernorm (Name: ${nodeName}, Op: ${nodeOp})
+PULP_Layernorm_fp${data_in_type.referencedType.typeWidth}_fp${data_out_type.referencedType.typeWidth}(
+    ${data_in}, 
+    ${data_out}, 
+    ${weight}, 
+    ${bias}, 
+    ${epsilon}, 
+    ${size}, 
+    ${lastDimLength}
+);
 """)
