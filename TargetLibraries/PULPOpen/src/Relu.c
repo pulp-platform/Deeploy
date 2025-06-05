d1516c20db46cc93ea8b0c2a213ed2cacd2660e3
Run Wang <52746141+SamanthaWangdl@users.noreply.github.com>
2025-06-05 21:57:01 +0000

    Relu parallel new template

---
+++ TargetLibraries/PULPOpen/src/Relu.c
@@ -0,0 +1,52 @@
+
+/* =====================================================================
+ * Title:        Relu.c
+ * Description:
+ *
+ * Date:         05.06.2025
+ *
+ * ===================================================================== */
+
+/*
+ * Copyright (C) 2022 ETH Zurich and University of Bologna.
+ *
+ * Authors:
+ * - Run Wang, ETH Zurich
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "DeeployPULPMath.h"
+#include "pmsis.h"
+
+
+void PULP_Relu_fp32_fp32(float32_t *input, float32_t *output, uint32_t size) {
+    
+    int8_t core_id = pi_core_id();
+    int8_t log2Core = log2(NUM_CORES);
+    
+    int32_t chunk = (size >> log2Core) + ((size & (NUM_CORES-1)) != 0);
+    int32_t start = MIN(chunk * core_id, size);
+    int32_t end = MIN(start + chunk, size);
+    int32_t local_size = end - start;
+
+
+    float32_t *local_input = input + start;
+    float32_t *local_output = output + start;
+    
+    for (int32_t i = 0; i < local_size; i++) {
+        local_output[i] = MAX(local_input[i], 0.0f);
+    }
+}
