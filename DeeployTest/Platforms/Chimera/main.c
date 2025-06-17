610b90d913c46f007ee9d3e5a88de7e18931e223
victor.jung <jungvi@iis.ee.ethz.ch>
2025-06-17 09:26:02 +0200

    Add Generic Test Harness for Chimera

--- DeeployTest/Platforms/Chimera/main.c
+++ DeeployTest/Platforms/Chimera/main.c
@@ -20,10 +20,10 @@ int main() {
 
     InitNetwork(0, 1);
 
-    // for (uint32_t buf = 0; buf < DeeployNetwork_num_inputs; buf++) {
-    //     memcpy(DeeployNetwork_inputs[buf], testInputVector[buf],
-    //         DeeployNetwork_inputs_bytes[buf]);
-    // }
+    for (uint32_t buf = 0; buf < DeeployNetwork_num_inputs; buf++) {
+        memcpy(DeeployNetwork_inputs[buf], testInputVector[buf],
+            DeeployNetwork_inputs_bytes[buf]);
+    }
 
     printf("Running network...\r\n");
     RunNetwork(0, 1);
@@ -33,29 +33,24 @@ int main() {
     OUTPUTTYPE diff;
     OUTPUTTYPE expected, actual;
 
-    // volatile int32_t a = ((OUTPUTTYPE *)DeeployNetwork_outputs[0])[0];
+    for (uint32_t buf = 0; buf < DeeployNetwork_num_outputs; buf++) {
+        tot += DeeployNetwork_outputs_bytes[buf] / sizeof(OUTPUTTYPE);
+        for (uint32_t i = 0;
+            i < DeeployNetwork_outputs_bytes[buf] / sizeof(OUTPUTTYPE); i++) {
+            expected = ((OUTPUTTYPE *)testOutputVector[buf])[i];
+            actual = ((OUTPUTTYPE *)DeeployNetwork_outputs[buf])[i];
+            diff = expected - actual;
 
-    printf("%p \n", DeeployNetwork_outputs[0]);
-    // printf("%d \n", ((OUTPUTTYPE *)DeeployNetwork_outputs[0])[0]);
+            if (diff != 0) {
+                tot_err += 1;
+                printf("Expected: %4d  ", expected);
+                printf("Actual: %4d  ", actual);
+                printf("Diff: %4d at Index %12u in Output %u\r\n", diff, i, buf);
+            }
+        }
+    }
 
-    // for (uint32_t buf = 0; buf < DeeployNetwork_num_outputs; buf++) {
-    //     tot += DeeployNetwork_outputs_bytes[buf] / sizeof(OUTPUTTYPE);
-    //     for (uint32_t i = 0;
-    //         i < DeeployNetwork_outputs_bytes[buf] / sizeof(OUTPUTTYPE); i++) {
-    //         expected = ((OUTPUTTYPE *)testOutputVector[buf])[i];
-    //         actual = ((OUTPUTTYPE *)DeeployNetwork_outputs[buf])[i];
-    //         diff = expected - actual;
-
-    //         if (diff != 0) {
-    //             tot_err += 1;
-    //             printf("Expected: %4d  ", expected);
-    //             printf("Actual: %4d  ", actual);
-    //             printf("Diff: %4d at Index %12u in Output %u\r\n", diff, i, buf);
-    //         }
-    //     }
-    // }
-
-    // printf("Errors: %d out of %d \r\n", tot_err, tot);
+    printf("Errors: %d out of %d \r\n", tot_err, tot);
 
     return 0;
 }
