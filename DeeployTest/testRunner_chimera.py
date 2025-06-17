461523ace0e6c86f607288bc55b7655b28ffe35b
victor.jung <jungvi@iis.ee.ethz.ch>
2025-06-17 13:26:14 +0200

    Add GVSoC Support for CMake Chimera platform

--- DeeployTest/testRunner_chimera.py
+++ DeeployTest/testRunner_chimera.py
@@ -30,8 +30,17 @@ if __name__ == "__main__":
     parser = TestRunnerArgumentParser(
         tiling_arguments = False,
         description = "Deeploy Code Generation Utility for the Chimera Platform (Host, no Tiling).")
+    
+    parser.add_argument('--simulator',
+                        metavar = "<simulator>",
+                        dest = "simulator",
+                        type = str,
+                        choices = ["gvsoc"], 
+                        default = "gvsoc",
+                        help = "Select the simulator to use")
+
     args = parser.parse_args()
 
-    testRunner = TestRunner(platform = "Chimera", simulator = "host", tiling = False, argument_parser = parser)
+    testRunner = TestRunner(platform = "Chimera", simulator = args.simulator, tiling = False, argument_parser = parser)
 
     testRunner.run()
