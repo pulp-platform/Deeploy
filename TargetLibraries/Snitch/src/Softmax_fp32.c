1209d861330de3baa2038f6e867917ca5c2a7b68
Philip Wiese <wiesep@iis.ee.ethz.ch>
2025-09-11 18:39:11 +0200

    Add missing C license header

--- TargetLibraries/Snitch/src/Softmax_fp32.c
+++ TargetLibraries/Snitch/src/Softmax_fp32.c
@@ -1,3 +1,29 @@
+/* ----------------------------------------------------------------------
+#
+# File: Softmax_fp32.c
+#
+# Last edited: 11.09.2025
+#
+# Copyright (C) 2025, ETH Zurich and University of Bologna.
+#
+# Author:
+# - Taha El Bayed, ETH Zurich
+#
+# ----------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the License); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
 #include "DeeploySnitchMath.h"
 
 void Softmax_fp32(float32_t *input, float32_t *output, int32_t ldI,
