aa44f51312c75f89a878b49b6337fb62cbb0809c
Luka Macan <luka.macan@unibo.it>
2025-09-20 09:34:51 +0200

    Refactor doublebuffering

--- Deeploy/TilingExtension/CodeTransformationPasses/DoubleBufferingTilingCodeGeneration.py
+++ Deeploy/TilingExtension/CodeTransformationPasses/DoubleBufferingTilingCodeGeneration.py
@@ -3,18 +3,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import copy
-import math
-from typing import List, Set, Tuple
+import itertools
+from typing import Dict, List, Set, Tuple
 
 from Deeploy.AbstractDataTypes import VoidType
 from Deeploy.DeeployTypes import CodeSnippet, ExecutionBlock, NetworkContext, NodeTemplate, OperatorRepresentation, \
     VariableBuffer, _ReferenceBuffer
-from Deeploy.TilingExtension.AsyncDma import AnydimAsyncDmaTransferAdapter, AsyncDma, Future
+from Deeploy.TilingExtension.AsyncDma import AsyncDma, DmaDirection, Future
 from Deeploy.TilingExtension.CodeTransformationPasses.TilingCodeGeneration import TilingCodeGeneration
 from Deeploy.TilingExtension.CodeTransformationPasses.TilingHoistingMixIn import dictOfArrays
 from Deeploy.TilingExtension.CodeTransformationPasses.TilingPrototypes import TilingMetaInfo
-from Deeploy.TilingExtension.MemoryConstraints import NodeMemoryConstraint
-from Deeploy.TilingExtension.TilingCodegen import TilingSchedule, VariableReplacementScheme, stridesFromShape
+from Deeploy.TilingExtension.MemoryConstraints import NodeMemoryConstraint, TensorMemoryConstraint
+from Deeploy.TilingExtension.TilingCodegen import HyperRectangle, TilingSchedule, VariableReplacementScheme
 
 
 class DoubleBufferingTilingCodeGeneration(TilingCodeGeneration):
@@ -60,132 +60,129 @@ class DoubleBufferingTilingCodeGeneration(TilingCodeGeneration):
             })
         ] for buff in buffers], tileIdxVar)
 
+    def _ioCalls(
+        self,
+        ctxt: NetworkContext,
+        operatorRepresentation: OperatorRepresentation,
+        schedule: List[Dict[str, HyperRectangle]],
+        memoryConstraints: Dict[str, TensorMemoryConstraint],
+        direction: DmaDirection,
+        tileIdxVar: str,
+        multibufferMap: Dict[str, List[_ReferenceBuffer]],
+    ) -> Tuple[List[CodeSnippet], Set[Future]]:
+        calls, futures = [], set()
+        for tensorName, rectangles in dictOfArrays(schedule).items():
+            localBuffer = ctxt.lookup(operatorRepresentation[tensorName])
+            assert localBuffer._memoryLevel == self.localMemory
+            assert isinstance(localBuffer, _ReferenceBuffer)
+            externalBuffer = ctxt.lookup(localBuffer._referenceName)
+            assert isinstance(externalBuffer, VariableBuffer)
+            tensorMemoryConstraint = memoryConstraints[externalBuffer.name]
+            externalBufferShape = tensorMemoryConstraint.memoryConstraints[self.externalMemory].shape
+            assert externalBufferShape is not None
+
+            rectangles, externalBufferShape = self._legalizeTransfers(rectangles, tuple(externalBufferShape),
+                                                                      localBuffer._type.referencedType.typeWidth,
+                                                                      self.isFinalMemoryLevel(tensorMemoryConstraint))
+
+            externalBufferRef = self._hoistReference(ctxt,
+                                                     externalBuffer.name + "_ref",
+                                                     externalBuffer,
+                                                     externalBufferShape,
+                                                     override_type = VoidType)
+
+            caseBlocks = []
+            for i, buff in enumerate(multibufferMap[tensorName]):
+                future = self.dma.getFuture(tensorName, copyIdx = i)
+                futures.add(future)
+
+                block = [future.alloc()]
+                block.extend(
+                    self._generateDmaTransferCalls(ctxt, tensorName, rectangles, tileIdxVar, buff, externalBufferRef,
+                                                   direction, future))
+                caseBlocks.append(block)
+
+            calls.extend(self._switch(caseBlocks, tileIdxVar))
+
+            referenceUpdate = self._generateExternalReferenceUpdate(ctxt, tensorName, rectangles, tileIdxVar,
+                                                                    externalBufferRef)
+            if referenceUpdate is not None:
+                calls.append(referenceUpdate)
+
+        return calls, futures
+
     def _tilingLoop(self, ctxt: NetworkContext, executionBlock: ExecutionBlock,
                     nodeMemoryConstraint: NodeMemoryConstraint, tilingSchedule: TilingSchedule,
                     variableReplacement: VariableReplacementScheme,
                     operatorRepresentation: OperatorRepresentation) -> Tuple[NetworkContext, ExecutionBlock, bool]:
 
-        setupStatements: List[CodeSnippet] = []
-        teardownStatements: List[CodeSnippet] = []
+        multibufferMap: Dict[str, List[_ReferenceBuffer]] = {}
 
-        openLoopStatements: List[CodeSnippet] = [CodeSnippet(self._openTileLoopTemplate, {**operatorRepresentation})]
+        for name in tilingSchedule.inputLoadSchedule[0].keys():
+            localBuffer = ctxt.lookup(operatorRepresentation[name])
+            assert isinstance(localBuffer, _ReferenceBuffer)
+            externalBuffer = ctxt.lookup(localBuffer._referenceName)
+            assert isinstance(externalBuffer, VariableBuffer)
+            tensorMemoryConstraint = nodeMemoryConstraint.inputTensorMemoryConstraints[externalBuffer.name]
+            buffers = self._hoistMultibufferReferences(ctxt, localBuffer, tensorMemoryConstraint)
+            multibufferMap[name] = buffers
+
+        for name in tilingSchedule.outputLoadSchedule[0].keys():
+            localBuffer = ctxt.lookup(operatorRepresentation[name])
+            assert isinstance(localBuffer, _ReferenceBuffer)
+            externalBuffer = ctxt.lookup(localBuffer._referenceName)
+            assert isinstance(externalBuffer, VariableBuffer)
+            tensorMemoryConstraint = nodeMemoryConstraint.outputTensorMemoryConstraints[externalBuffer.name]
+            buffers = self._hoistMultibufferReferences(ctxt, localBuffer, tensorMemoryConstraint)
+            multibufferMap[name] = buffers
+
+        openLoopStatements: List[CodeSnippet] = [CodeSnippet(self._openTileLoopTemplate, operatorRepresentation)]
+
+        bufferGroupChoices = [[] for _ in range(self.bufferCount)]
+        for name in itertools.chain(tilingSchedule.inputLoadSchedule[0].keys(),
+                                    tilingSchedule.outputLoadSchedule[0].keys()):
+            localBuffer = ctxt.lookup(operatorRepresentation[name])
+            assert isinstance(localBuffer, _ReferenceBuffer)
+            for i, buffer in enumerate(multibufferMap[name]):
+                snippet = CodeSnippet(self._referenceUpdate, {
+                    "reference": localBuffer.name,
+                    "type": localBuffer._type.typeName,
+                    "update": buffer.name
+                })
+                bufferGroupChoices[i].append(snippet)
+
+        openLoopStatements += self._switch(bufferGroupChoices, "TILING_I")
+
+        ingressCalls, ingressFutures = self._ioCalls(ctxt, operatorRepresentation, tilingSchedule.inputLoadSchedule,
+                                                     nodeMemoryConstraint.inputTensorMemoryConstraints,
+                                                     "ExternalToLocal", "TILING_I+1", multibufferMap)
 
         ingressDmaTransferCalls: List[CodeSnippet] = [
             CodeSnippet(self._moveTileInCheckOpenStatement, {
                 **operatorRepresentation, "tileIdxVar": "TILING_I+1"
             })
-        ]
-
-        ingressFutures: Set[Future] = set()
-        initialFutures: Set[Future] = set()
-
-        for tensorName, rectangles in dictOfArrays(tilingSchedule.inputLoadSchedule).items():
-            localBuffer = ctxt.lookup(operatorRepresentation[tensorName])
-            assert localBuffer._memoryLevel == self.localMemory
-            assert isinstance(localBuffer, _ReferenceBuffer)
-            externalBuffer = ctxt.lookup(localBuffer._referenceName)
-            assert isinstance(externalBuffer, VariableBuffer)
-            tensorMemoryConstraint = nodeMemoryConstraint.inputTensorMemoryConstraints[externalBuffer.name]
-            externalBufferShape = tensorMemoryConstraint.memoryConstraints[self.externalMemory].shape
-            assert externalBufferShape is not None
-
-            rectangles, externalBufferShape = self._legalizeTransfers(rectangles, tuple(externalBufferShape),
-                                                                      localBuffer._type.referencedType.typeWidth,
-                                                                      self.isFinalMemoryLevel(tensorMemoryConstraint))
-
-            externalBufferRef = self._hoistReference(ctxt,
-                                                     externalBuffer.name + "_ref",
-                                                     externalBuffer,
-                                                     externalBufferShape,
-                                                     override_type = VoidType)
-
-            tensorMemoryConstraint = nodeMemoryConstraint.inputTensorMemoryConstraints[externalBuffer.name]
-            l1BuffersReferences = self._hoistMultibufferReferences(ctxt, localBuffer, tensorMemoryConstraint)
-
-            nextLocalBufferReference = self._hoistReference(ctxt, f"{tensorName}_next", l1BuffersReferences[1])
-
-            openLoopStatements.extend(self._generateBufferChoice(localBuffer, l1BuffersReferences, "TILING_I"))
-
-            future = self.dma.getFuture(tensorName)
-            ingressFutures.add(future)
-
-            ingressDmaTransferCalls.extend(
-                self._generateBufferChoice(nextLocalBufferReference, l1BuffersReferences, "TILING_I+1"))
-            ingressDmaTransferCalls.extend(
-                self._generateDmaTransferCalls(ctxt, tensorName, rectangles, "TILING_I+1", nextLocalBufferReference,
-                                               externalBufferRef, "ExternalToLocal", future))
-
-            anydimAdapter = AnydimAsyncDmaTransferAdapter(self.dma)
-
-            initialFuture = self.dma.getFuture(tensorName + "_init")
-            initialFutures.add(initialFuture)
-            initialDmaTransferCalls = anydimAdapter.transfer(ctxt, externalBufferRef, localBuffer, rectangles[0].dims,
-                                                             stridesFromShape(externalBufferShape),
-                                                             stridesFromShape(rectangles[0].dims), "ExternalToLocal",
-                                                             initialFuture, math.prod(externalBufferShape))
-            setupStatements.extend(initialDmaTransferCalls)
-            setupStatements.append(initialFuture.wait())
-
-            referenceUpdate = self._generateExternalReferenceUpdate(ctxt, tensorName, rectangles, "TILING_I+1",
-                                                                    externalBufferRef)
-            if referenceUpdate is not None:
-                ingressDmaTransferCalls.append(referenceUpdate)
-                initialReferenceUpdate = CodeSnippet(referenceUpdate.template,
-                                                     operatorRepresentation = {
-                                                         **referenceUpdate.operatorRepresentation,
-                                                         "tileIdxVar": 0,
-                                                     })
-                setupStatements.append(initialReferenceUpdate)
-
-        ingressDmaTransferCalls.append(CodeSnippet(self._blockClose, {}))
+        ] + ingressCalls + [CodeSnippet(self._blockClose, {})]
         ingressDmaWaitStatements = [f.wait() for f in ingressFutures]
 
-        egressDmaTransferCalls: List[CodeSnippet] = []
-        egressFutures: Set[Future] = set()
+        firstIngressCalls = []
+        for snippet in ingressCalls:
+            tmpl, opRepr = snippet.template, snippet.operatorRepresentation
+            firstIngressCalls.append(CodeSnippet(tmpl, {**opRepr, "tileIdxVar": 0}))
 
-        for tensorName, rectangles in dictOfArrays(tilingSchedule.outputLoadSchedule).items():
-            localBuffer = ctxt.lookup(operatorRepresentation[tensorName])
-            assert localBuffer._memoryLevel == self.localMemory
-            assert isinstance(localBuffer, _ReferenceBuffer)
-            externalBuffer = ctxt.lookup(localBuffer._referenceName)
-            assert isinstance(externalBuffer, VariableBuffer)
-            tensorMemoryConstraint = nodeMemoryConstraint.outputTensorMemoryConstraints[externalBuffer.name]
-            externalBufferShape = tensorMemoryConstraint.memoryConstraints[self.externalMemory].shape
-            assert externalBufferShape is not None
+        egressCalls, egressFutures = self._ioCalls(ctxt, operatorRepresentation, tilingSchedule.outputLoadSchedule,
+                                                   nodeMemoryConstraint.outputTensorMemoryConstraints,
+                                                   "LocalToExternal", "TILING_I", multibufferMap)
 
-            rectangles, externalBufferShape = self._legalizeTransfers(rectangles, tuple(externalBufferShape),
-                                                                      localBuffer._type.referencedType.typeWidth,
-                                                                      self.isFinalMemoryLevel(tensorMemoryConstraint))
-
-            externalBufferRef = self._hoistReference(ctxt,
-                                                     externalBuffer.name + "_ref",
-                                                     externalBuffer,
-                                                     externalBufferShape,
-                                                     override_type = VoidType)
-
-            tensorMemoryConstraint = nodeMemoryConstraint.outputTensorMemoryConstraints[externalBuffer.name]
-            l1BuffersReferences = self._hoistMultibufferReferences(ctxt, localBuffer, tensorMemoryConstraint)
-
-            openLoopStatements.extend(self._generateBufferChoice(localBuffer, l1BuffersReferences, "TILING_I"))
-
-            future = self.dma.getFuture(tensorName)
-            egressFutures.add(future)
-
-            dmaTransferCalls = self._generateDmaTransferCalls(ctxt, tensorName, rectangles, "TILING_I", localBuffer,
-                                                              externalBufferRef, "LocalToExternal", future)
-            egressDmaTransferCalls.extend(dmaTransferCalls)
-
-            referenceUpdate = self._generateExternalReferenceUpdate(ctxt, tensorName, rectangles, "TILING_I",
-                                                                    externalBufferRef)
-            if referenceUpdate is not None:
-                egressDmaTransferCalls.append(referenceUpdate)
-
+        egressDmaTransferCalls: List[CodeSnippet] = egressCalls
         egressDmaWaitStatements = [f.wait() for f in egressFutures]
 
-        teardownStatements.extend([f.wait() for f in egressFutures])
+        setupStatements: List[CodeSnippet] = []
+        setupStatements += [f.init() for f in ingressFutures | egressFutures] + setupStatements
+        setupStatements.extend(firstIngressCalls)
 
-        setupStatements = [f.init() for f in ingressFutures | initialFutures | egressFutures] + setupStatements
-        teardownStatements.extend(f.deinit() for f in ingressFutures | initialFutures | egressFutures)
+        teardownStatements: List[CodeSnippet] = []
+        teardownStatements.extend([f.wait() for f in egressFutures])
+        teardownStatements.extend(f.deinit() for f in ingressFutures | egressFutures)
 
         closeLoopStatements = [CodeSnippet(self._closeTileLoopTemplate, {**operatorRepresentation})]
 
