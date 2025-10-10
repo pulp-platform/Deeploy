19810aff1213266966c0ff890f403bcad83799b8
diaco <diaconuccalin@gmail.com>
2025-10-10 16:08:16 +0000

    Updated PULP GELU function to generic version (while keeping the parallelization)

--- TargetLibraries/PULPOpen/src/GELU.c
+++ TargetLibraries/PULPOpen/src/GELU.c
@@ -12,23 +12,25 @@
 
 void PULP_GELU_fp32_fp32(float32_t *data_in, float32_t *data_out,
                          int32_t dataSize) {
+  // Get core information
   int8_t core_id = pi_core_id();
   int8_t log2Core = LOG2(NUM_CORES);
+
+  // Split into chunks for each core
   int16_t chunk = (dataSize >> log2Core) + ((dataSize & (NUM_CORES - 1)) != 0);
   int16_t chunk_start = MIN(chunk * core_id, dataSize);
   int16_t chunk_stop = MIN(chunk_start + chunk, dataSize);
+
+  // Set constants
   const float32_t sqrt_2_over_pi = 0.7978845608f; // sqrt(2/π)
   const float32_t coeff = 0.044715f;
 
+  // Compute GELU on the assigned chunk
   for (uint32_t i = chunk_start; i < chunk_stop; i++) {
     float32_t x = data_in[i];
-    float32_t x_cubed = x * x * x;
-    float32_t inner = sqrt_2_over_pi * (x + coeff * x_cubed);
+    float32_t cdf = 0.5f * (1.0f + tanhf((sqrtf(2.0f / (float)M_PI) *
+                                          (x + 0.044715f * powf(x, 3.0f)))));
 
-    float32_t exp_2z = expf(2.0f * inner);
-    float32_t tanh_val = (exp_2z - 1.0f) / (exp_2z + 1.0f);
-
-    float32_t cdf = 0.5f * (1.0f + tanh_val);
     data_out[i] = x * cdf;
   }
 }
