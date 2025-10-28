444064d9ae04d3828aebbb913c027df744d22067
Luka Macan <luka.macan@unibo.it>
2025-10-28 17:48:10 +0100

    SignPropDeployer changes

--- Deeploy/CommonExtensions/NetworkDeployers/SignPropDeployer.py
+++ Deeploy/CommonExtensions/NetworkDeployers/SignPropDeployer.py
@@ -6,8 +6,10 @@ from typing import Callable, Dict, Type
 
 import onnx_graphsurgeon as gs
 
-from Deeploy.AbstractDataTypes import Pointer
-from Deeploy.DeeployTypes import DeploymentPlatform, NetworkDeployer, OperatorDescriptor, TopologyOptimizer
+from Deeploy.AbstractDataTypes import IntegerImmediate, Pointer
+from Deeploy.CommonExtensions.TypeCheckers.SignPropTypeChecker import SignPropTypeChecker
+from Deeploy.DeeployTypes import ConstantBuffer, DeploymentPlatform, NetworkDeployer, OperatorDescriptor, \
+    TopologyOptimizer, VariableBuffer
 from Deeploy.Logging import DEFAULT_LOGGER as log
 
 
@@ -33,17 +35,6 @@ class SignPropDeployer(NetworkDeployer):
 
         self.inputOffsets = inputOffsets
 
-    def _createIOBindings(self, ctxt, graph):
-        ctxt = super()._createIOBindings(ctxt, graph)
-        for node in graph.inputs:
-            data_name = node.name
-            nb = ctxt.lookup(data_name)
-            data_type = self.inputTypes[data_name]
-            nb._signed = (self.inputOffsets[data_name] == 0)
-            nb.nLevels = (2**data_type.referencedType.typeWidth)
-
-        return ctxt
-
     def _printInputOutputSummary(self):
         log.info('Input:')
         for buf in self.inputs():
@@ -56,3 +47,39 @@ class SignPropDeployer(NetworkDeployer):
             log.info(
                 f" - '{buf.name}': Type: {buf._type.referencedType.typeName}, nLevels: {buf.nLevels}, Signed: {buf._signed}"
             )
+
+    def parse(self, default_channels_first: bool = True) -> bool:
+        parsable = super().parse(default_channels_first)
+        if not parsable:
+            return False
+
+        # Annotate global buffers
+        for obj in self.ctxt.globalObjects.values():
+            assert isinstance(obj, VariableBuffer)
+            refTy = obj._type.referencedType
+            if isinstance(obj, ConstantBuffer):
+                assert refTy.checkPromotion(obj.values), f"Can't cast {obj} to {refTy}"
+                if issubclass(refTy, IntegerImmediate):
+                    obj.nLevels = obj.values.max() - obj.values.min()
+                    obj._signed = refTy.typeMin < 0
+            elif obj.name in self.inputOffsets:
+                obj._signed = (self.inputOffsets[obj.name] == 0)
+                obj.nLevels = (2**refTy.typeWidth)
+
+        # Annotate rest
+        for layer in self.layerBinding.values():
+            node = layer.node
+            opRepr = layer.mapper.parser.operatorRepresentation
+            typeChecker = layer.mapper.binder.typeChecker
+            outTy = self.ctxt.lookup(node.outputs[0].name)._type.referencedType
+            if issubclass(outTy, IntegerImmediate) and isinstance(typeChecker, SignPropTypeChecker):
+                inputs = [self.ctxt.lookup(t.name) for t in node.inputs]
+                outputNLevels = typeChecker._inferNumLevels(inputs, opRepr)
+                outputSigned = typeChecker._inferSignedness(inputs, opRepr)
+
+                outputs = [self.ctxt.lookup(t.name) for t in node.outputs]
+                for buffer, nLevels, signed in zip(outputs, outputNLevels, outputSigned):
+                    buffer.nLevels = nLevels
+                    buffer._signed = signed
+
+        return True
