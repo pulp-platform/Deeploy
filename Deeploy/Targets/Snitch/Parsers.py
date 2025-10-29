38de2389aaef1347269f19144084872440e9b3d6
Luka Macan <luka.macan@unibo.it>
2025-10-29 11:45:49 +0100

    Move iNoNorm from Generic to Snitch since it's only used there

--- Deeploy/Targets/Snitch/Parsers.py
+++ Deeploy/Targets/Snitch/Parsers.py
@@ -2,11 +2,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import math
 from typing import Tuple
 
 import onnx_graphsurgeon as gs
 
-from Deeploy.DeeployTypes import NetworkContext
+from Deeploy.DeeployTypes import NetworkContext, NodeParser
 from Deeploy.Targets.Generic.Parsers import GEMMParser, RQGEMMParser
 
 
@@ -68,3 +69,36 @@ class SnitchRQGEMMParser(RQGEMMParser):
             return ctxt, False
 
         return newCtxt, True
+
+
+class iNoNormParser(NodeParser):
+
+    def __init__(self):
+        super().__init__()
+
+    def parseNode(self, node: gs.Node) -> bool:
+
+        ret = all(['D' in node.attrs, 'mul' in node.attrs, 'n_levels' in node.attrs])
+
+        if ret:
+            self.operatorRepresentation.update(node.attrs)
+            self.operatorRepresentation['log2D'] = int(math.log2(node.attrs['D']))
+
+        return ret
+
+    def parseNodeCtxt(self,
+                      ctxt: NetworkContext,
+                      node: gs.Node,
+                      channels_first: bool = True) -> Tuple[NetworkContext, bool]:
+
+        data_in = ctxt.lookup(node.inputs[0].name)
+        weights = ctxt.lookup(node.inputs[1].name)
+        bias = ctxt.lookup(node.inputs[2].name)
+        data_out = ctxt.lookup(node.outputs[0].name)
+        self.operatorRepresentation['data_in'] = data_in.name
+        self.operatorRepresentation['weights'] = weights.name
+        self.operatorRepresentation['bias'] = bias.name
+        self.operatorRepresentation['data_out'] = data_out.name
+        self.operatorRepresentation['size'] = math.prod(data_in.shape)
+
+        return ctxt, True
