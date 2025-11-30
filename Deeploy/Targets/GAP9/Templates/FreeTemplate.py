ccaad61e63aedcf84c108666cdf4ffeab98d8d7e
Run Wang <52746141+SamanthaWangdl@users.noreply.github.com>
2025-11-30 20:01:12 +0000

    [GAP9] Fix free template from PULP-sdk to GAP9sdk

--- Deeploy/Targets/GAP9/Templates/FreeTemplate.py
+++ Deeploy/Targets/GAP9/Templates/FreeTemplate.py
@@ -6,12 +6,12 @@ from Deeploy.DeeployTypes import NodeTemplate
 
 gap9L2LocalTemplate = NodeTemplate("pi_l2_free(${name}, sizeof(${type.referencedType.typeName}) * ${size});")
 gap9L2GlobalTemplate = NodeTemplate("pi_l2_free(${name}, sizeof(${type.referencedType.typeName}) * ${size});")
-gap9L1FreeTemplate = NodeTemplate("pi_l1_malloc_free(${name}, sizeof(${type.referencedType.typeName}) * ${size});\n")
+gap9L1FreeTemplate = NodeTemplate("pi_l1_free(${name}, sizeof(${type.referencedType.typeName}) * ${size});\n")
 gap9L1GlobalFreeTemplate = NodeTemplate("")
 
 gap9GenericFree = NodeTemplate("""
 % if _memoryLevel == "L1":
-pi_l1_malloc_free(${name}, sizeof(${type.referencedType.typeName}) * ${size});
+pi_l1_free(${name}, sizeof(${type.referencedType.typeName}) * ${size});
 % elif _memoryLevel == "L2" or _memoryLevel is None:
 pi_l2_free(${name}, sizeof(${type.referencedType.typeName}) * ${size});
 % elif _memoryLevel == "L3":
