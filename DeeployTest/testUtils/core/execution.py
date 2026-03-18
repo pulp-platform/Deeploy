983eccab5486bac817a1e48184566e0d1322221f
Run Wang <52746141+SamanthaWangdl@users.noreply.github.com>
2026-03-18 21:17:24 +0000

    Revert "Move gap9 binary to /bin"
    This reverts commit 7ba871c969a3a8fa35a34c1bce0a416c0202d999.

--- DeeployTest/testUtils/core/execution.py
+++ DeeployTest/testUtils/core/execution.py
@@ -374,10 +374,10 @@ def run_simulation(config: DeeployTestConfig, skip: bool = False) -> TestResult:
         _GVSOC_TARGET_MAP = {'gap9': 'gap9.evk'}
         gvsoc_target = _GVSOC_TARGET_MAP.get(platform_lower, platform_lower)
 
-        # Binary is always placed in build_dir/bin/ (CMAKE_RUNTIME_OUTPUT_DIRECTORY = CMAKE_BINARY_DIR/bin).
+        # Binary is placed directly in build_dir (not in a bin/ subdir).
         # For GAP9, the image target also produces .bin files that gvsoc needs
         # in the work directory; copy them if present.
-        binary_path = str(Path(config.build_dir) / 'bin' / config.test_name)
+        binary_path = str(Path(config.build_dir) / config.test_name)
         for bin_file in Path(config.build_dir).glob('*.bin'):
             shutil.copy2(str(bin_file), workdir)
         gap9_sdk = env.get('GAP_SDK_HOME', '')
