6e60c6c6540f0c7f07e41ff987f9dbdd9d2f35cd
Victor Jung <jungvi@iis.ee.ethz.ch>
2026-04-08 16:01:40 +0200

    format

--- TargetLibraries/XDNA2/aie_kernel_utils.h
+++ TargetLibraries/XDNA2/aie_kernel_utils.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (C) 2025 Advanced Micro Devices, Inc. All
+// rights reserved. SPDX-License-Identifier: Apache-2.0
+
 #ifndef _AIE_KERNEL_UTILS_
 #define _AIE_KERNEL_UTILS_
 
