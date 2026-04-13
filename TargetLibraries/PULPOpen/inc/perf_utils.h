3d1b6afbadd104cd997d1aa6ef39ad6db07c3db8
Run Wang <samanthawangdl@gmail.com>
2026-04-13 10:59:30 +0000

    perf-util add pre-commit

--- TargetLibraries/PULPOpen/inc/perf_utils.h
+++ TargetLibraries/PULPOpen/inc/perf_utils.h
@@ -1,4 +1,8 @@
 /*
+ * SPDX-FileCopyrightText: 2025 ETH Zurich and University of Bologna
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Performance Counter Utilities for PULP Benchmarking
  */
 
