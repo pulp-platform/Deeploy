cdfa5e036d8c65b08f39e587c7e8fe054710c1f5
bowwang <bowwang@iis.ee.ethz.ch>
2026-04-23 16:04:17 +0200

    [template] Add proper allocation template and fix DMA template

--- Deeploy/Targets/Spatz/DMA/SpatzDma.py
+++ Deeploy/Targets/Spatz/DMA/SpatzDma.py
@@ -35,8 +35,6 @@ class SpatzDma(AsyncDma):
             NodeTemplate("""
             if (snrt_is_dm_core()) {
                 ${future} = snrt_dma_start_2d(${dest}, ${src}, ${size}, ${stride_dest}, ${stride_src}, ${repeat});
-                // WIESEP: Hack as otherwise the last commited DMA transaction ID can never be resolved.
-                snrt_dma_start_2d(${dest}, ${dest}, 1, 0, 0, 0);
             }
             """),
     }
--- Deeploy/Targets/Spatz/Templates/AllocateTemplate.py
+++ Deeploy/Targets/Spatz/Templates/AllocateTemplate.py
@@ -3,3 +3,14 @@ from Deeploy.DeeployTypes import NodeTemplate
 # allocate
 referenceAllocateTemplate = NodeTemplate(
     "${name} = (${type.typeName}) snrt_l1alloc(${type.referencedType.typeWidth//8} * ${size});\n")
+
+spatzGenericAllocate = NodeTemplate("""
+% if _memoryLevel == "L1":
+${name} = (${type.typeName}) snrt_l1alloc(sizeof(${type.referencedType.typeName}) * ${size});\n
+% elif _memoryLevel == "L3" or _memoryLevel is None:
+${name} = (${type.typeName}) snrt_l3alloc(sizeof(${type.referencedType.typeName}) * ${size});\n
+% else:
+// COMPILER WARNING — unsupported memory level ${_memoryLevel}, defaulting to L3                                                                
+${name} = (${type.typeName}) snrt_l3alloc(${type.referencedType.typeWidth//8} * ${size});                                                       
+% endif 
+""")
