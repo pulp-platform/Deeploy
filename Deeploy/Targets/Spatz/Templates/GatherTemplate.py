878a3fd3383e06ffdf9155467ddc0d3b7eed8962
Bumblebee00 <sem26f13@badile103.ee.ethz.ch>
2026-04-23 21:53:22 +0200

    improved gather template

--- Deeploy/Targets/Spatz/Templates/GatherTemplate.py
+++ Deeploy/Targets/Spatz/Templates/GatherTemplate.py
@@ -1,5 +1,6 @@
 from Deeploy.DeeployTypes import NodeTemplate
 
+# TODO for l3 -> l3 transfers in spatz should memcpy be used?
 referenceTemplate = NodeTemplate("""
 // Gather (Name: ${nodeName}, Op: ${nodeOp})
 <%
@@ -9,6 +10,12 @@ BEGIN_SINGLE_CORE
 % if num_indices == 1:
 for (uint32_t i=0; i<${batch}; ++i) {
     snrt_dma_start_1d(${data_out} + i * ${axis_length}, ${data_in} + i * ${batch_length} + ${index} * ${axis_length}, ${axis_length} * ${width});
+}
+% elif batch==1:
+for (uint32_t j=0; j<${num_indices}; ++j) {
+    snrt_dma_start_1d(${data_out} + j * ${axis_length}, 
+            ${data_in} + ${indices}[j] * ${axis_length}, 
+            ${axis_length} * ${width});
 }
 % else:
 for (uint32_t i=0; i<${batch}; ++i) {
