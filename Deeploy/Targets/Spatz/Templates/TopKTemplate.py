ca2112c42721d6645e5f69b35c7aa0e921ab9158
Bumblebee00 <sem26f13@badile103.ee.ethz.ch>
2026-04-28 13:29:00 +0200

    fixed topk template

--- Deeploy/Targets/Spatz/Templates/TopKTemplate.py
+++ Deeploy/Targets/Spatz/Templates/TopKTemplate.py
@@ -4,35 +4,34 @@ from Deeploy.DeeployTypes import NetworkContext, NodeTemplate, OperatorRepresent
 
 
 SpatzTilingTemplate = NodeTemplate("""
-// TopK (Name: ${nodeName}, Op: ${nodeOp})
-// Find the top ${k_value} values and their indices
-// Assumes 1D input for simplicity
-typedef struct {
-	${data_in_type.referencedType.typeName} value;
-	uint32_t index;
-} topk_pair_t;
+// TopK node: finds the top ${k_value} values and their indices
+// Assumes 1D input 
+${data_in_type.referencedType.typeName} *values_tmp = snrt_l1alloc(sizeof(${data_in_type.referencedType.typeName})*${data_in_size});
+${indices_out_type.referencedType.typeName} *indices_tmp = snrt_l1alloc(sizeof(${indices_out_type.referencedType.typeName})*${data_in_size});
 
-topk_pair_t pairs[${data_in_size}];
 for (uint32_t i = 0; i < ${data_in_size}; ++i) {
-	pairs[i].value = ((${data_in_type.referencedType.typeName}*)${data_in})[i];
-	pairs[i].index = i;
+	values_tmp[i] = ((${data_in_type.referencedType.typeName}*)${data_in})[i];
+	indices_tmp[i] = i;
 }
 // Simple selection sort for top-k
 for (uint32_t i = 0; i < ${k_value}; ++i) {
 	uint32_t max_idx = i;
 	for (uint32_t j = i + 1; j < ${data_in_size}; ++j) {
-		if (pairs[j].value > pairs[max_idx].value) {
-			max_idx = j;
-		}
+        if (values_tmp[j] > values_tmp[max_idx]) {
+          max_idx = j;
+        }
 	}
 	// Swap
 	if (max_idx != i) {
-		topk_pair_t tmp = pairs[i];
-		pairs[i] = pairs[max_idx];
-		pairs[max_idx] = tmp;
+		float32_t tmp_val = values_tmp[i];
+		int32_t tmp_idx = indices_tmp[i];
+		values_tmp[i] = values_tmp[max_idx];
+		indices_tmp[i] = indices_tmp[max_idx];
+		values_tmp[max_idx] = tmp_val;
+		indices_tmp[max_idx] = tmp_idx;
 	}
 	// Write output
-	((${values_out_type.referencedType.typeName}*)${values_out})[i] = pairs[i].value;
-	((${indices_out_type.referencedType.typeName}*)${indices_out})[i] = pairs[i].index;
+	((${values_out_type.referencedType.typeName}*)${values_out})[i] = values_tmp[i];
+	((${indices_out_type.referencedType.typeName}*)${indices_out})[i] = indices_tmp[i];
 }
 """)
