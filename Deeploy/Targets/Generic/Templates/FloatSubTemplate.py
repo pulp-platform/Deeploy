4df67be264e8ba228a770422ed4a7ceca2217bf4
Alex Marchioni <alex.marchioni@outlook.it>
2026-05-22 08:26:13 +0000

    update dates in the headers

--- Deeploy/Targets/Generic/Templates/FloatSubTemplate.py
+++ Deeploy/Targets/Generic/Templates/FloatSubTemplate.py
@@ -1,11 +1,11 @@
-# SPDX-FileCopyrightText: 2021 ETH Zurich and University of Bologna
+# SPDX-FileCopyrightText: 2026 ETH Zurich and University of Bologna
 #
 # SPDX-License-Identifier: Apache-2.0
 
 from Deeploy.DeeployTypes import NodeTemplate
 
 referenceTemplate = NodeTemplate("""
-// Add (Name: ${nodeName}, Op: ${nodeOp})
+// Sub (Name: ${nodeName}, Op: ${nodeOp})
 BEGIN_SINGLE_CORE
     for (uint32_t i=0;i<${size};i++){
         ${data_out}[i] = ${data_in_1}[i] - ${data_in_2}[i];
