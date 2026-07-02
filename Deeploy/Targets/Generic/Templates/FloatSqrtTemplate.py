a30354a7edc2ec56d38e8c1f704357740e241118
Alex Marchioni <alex.marchioni@outlook.it>
2026-07-02 13:32:36 +0000

    minor refactor

--- Deeploy/Targets/Generic/Templates/FloatSqrtTemplate.py
+++ Deeploy/Targets/Generic/Templates/FloatSqrtTemplate.py
@@ -1,25 +1,9 @@
 # SPDX-FileCopyrightText: 2025 ETH Zurich and University of Bologna
 #
 # SPDX-License-Identifier: Apache-2.0
-from typing import Dict, List, Tuple
-
-from Deeploy.DeeployTypes import NetworkContext, OperatorRepresentation
 from Deeploy.Targets.Generic.Templates.FloatUnaryTemplate import _FloatUnaryTemplate
 
-
-class _SqrtTemplate(_FloatUnaryTemplate):
-
-    def alignToContext(self, ctxt: NetworkContext,
-                       operatorRepresentation: OperatorRepresentation) -> Tuple[NetworkContext, Dict, List[str]]:
-        ctxt, operatorRepresentation, deps = super().alignToContext(ctxt, operatorRepresentation)
-
-        data_in = ctxt.lookup(operatorRepresentation['data_in'])
-        operatorRepresentation['data_type'] = data_in._type.typeName
-
-        return ctxt, operatorRepresentation, deps
-
-
-referenceTemplate = _SqrtTemplate("""
+referenceTemplate = _FloatUnaryTemplate("""
 // Sqrt (Name: ${nodeName}, Op: ${nodeOp})
 Sqrt_fp${type_width}_fp${type_width}(${data_in}, ${data_out}, ${size});
 """)
